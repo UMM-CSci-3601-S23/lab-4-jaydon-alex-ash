a5e3953e6c32c3edd046d70c5ed8c481ea3a643f
Jay <stani152@morris.umn.edu>
2023-03-03 20:23:29 -0600

    Add page for adding todos on the client
    Co-authored-by: Matt Wanner <wanne051@morris.umn.edu>

--- client/src/app/todos/add-todo.component.ts
+++ client/src/app/todos/add-todo.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.scss']
 })
-export class AddTodoComponent {
+export class AddTodoComponent implements OnInit {
+
+  addTodoForm: UntypedFormGroup;
+
+  todo: Todo;
+
+    // not sure if this owner is magical and making it be found or if I'm missing something,
+  // but this is where the red text that shows up (when there is invalid input) comes from
+  addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+    ],
+
+    body: [
+      {type: 'required', message: 'Body is required'},
+      {type: 'minlength', message: 'Body must be at least 5 characters long'},
+      {type: 'maxlength', message: 'Body may not be more than 200 characters long'},
+    ],
+
+    status: [
+      {type: 'status', message: 'Status must be either complete or incomplete'},
+      {type: 'required', message: 'Status is required'}
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 3 characters long'},
+      {type: 'maxlength', message: 'Category may not be more than 30 characters long'},
+    ]
+  };
+
+  constructor(private fb: UntypedFormBuilder, private todoService: TodoService, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  createForms() {
+
+    // add todo form validations
+    this.addTodoForm = this.fb.group({
+      // We allow alphanumeric input and limit the length for owner.
+      owner: new UntypedFormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        // In the real world you'd want to be very careful about having
+        // an upper limit like this because people can sometimes have
+        // very long owners. This demonstrates that it's possible, though,
+        // to have maximum length limits.
+        Validators.maxLength(50),
+      ])),
+
+      body: new UntypedFormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(200),
+      ])),
+
+      // We don't care much about what is in the company field, so we just add it here as part of the form
+      // without any particular validation.
+      status: new UntypedFormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(complete|incomplete)$')
+      ])),
+
+      // We don't need a special validator just for our app here, but there is a default one for email.
+      // We will require the email, though.
+      category: new UntypedFormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+      ])),
+    });
+
+  }
+
+  ngOnInit() {
+    this.createForms();
+  }
+
+
+  submitForm() {
+    this.todoService.addTodo(this.addTodoForm.value).subscribe({
+      next: (newID) => {
+        this.snackBar.open(
+          `Added todo ${this.addTodoForm.value.owner}`,
+          null,
+          { duration: 2000 }
+        );
+        this.router.navigate(['/todos/', newID]);
+      },
+      error: err => {
+        this.snackBar.open(
+          'Failed to add the todo',
+          'OK',
+          { duration: 5000 }
+        );
+      },
+      // complete: () => console.log('Add todo completes!')
+    });
+  }
 
 }
